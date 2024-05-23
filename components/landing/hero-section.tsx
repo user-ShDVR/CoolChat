f3e2947a9babcaf425091d5c6444eb2fb30b606f
Miroslav <bangwars3@gmail.com>
2024-05-24 00:05:39 +0400

    first commit

---
+++ components/landing/hero-section.tsx
@@ -0,0 +1,63 @@
+"use client";
+
+import { BorderBeam } from "@/components/magicui/border-beam";
+import TextShimmer from "@/components/magicui/text-shimmer";
+import { Button } from "@/components/ui/button";
+import { ArrowRightIcon } from "@radix-ui/react-icons";
+import { useInView } from "framer-motion";
+import { useRef } from "react";
+
+export default function HeroSection() {
+  const ref = useRef(null);
+  const inView = useInView(ref, { once: true, margin: "-100px" });
+  return (
+    <section
+      id="hero"
+      className="relative mx-auto mt-32 max-w-[80rem] px-6 text-center md:px-8"
+    >
+      <div className="backdrop-filter-[12px] inline-flex h-7 items-center justify-between rounded-full border border-white/5 bg-white/10 px-3 text-xs text-white transition-all ease-in hover:cursor-pointer hover:bg-white/20 group gap-1 translate-y-[-1rem] animate-fade-in opacity-0">
+        <TextShimmer className="inline-flex items-center justify-center">
+          <span>✨ Представляем вам CoolChat</span>{" "}
+          <ArrowRightIcon className="ml-1 size-3 transition-transform duration-300 ease-in-out group-hover:translate-x-0.5" />
+        </TextShimmer>
+      </div>
+      <h1 className="bg-gradient-to-br from-white from-30% to-white/40 bg-clip-text py-6 text-5xl font-medium leading-none tracking-tighter text-transparent text-balance sm:text-6xl md:text-7xl lg:text-8xl translate-y-[-1rem] animate-fade-in opacity-0 [--animation-delay:200ms]">
+        CoolChat новый взгляд на
+        <br className="hidden md:block" /> обмен сообщениями.
+      </h1>
+      <p className="mb-12 text-lg tracking-tight text-gray-400 md:text-xl text-balance translate-y-[-1rem] animate-fade-in opacity-0 [--animation-delay:400ms]">
+        Данный проект создан на
+        <br className="hidden md:block" /> 
+        Nest.js, Next.js
+      </p>
+      <Button className="translate-y-[-1rem] animate-fade-in gap-1 rounded-lg text-black opacity-0 ease-in-out [--animation-delay:600ms]">
+        <span>Начинай чатится прямо сейчас</span>
+        <ArrowRightIcon className="ml-1 size-4 transition-transform duration-300 ease-in-out group-hover:translate-x-1" />
+      </Button>
+      <div
+        ref={ref}
+        className="relative mt-[8rem] animate-fade-up opacity-0 [--animation-delay:400ms] [perspective:2000px] after:absolute after:inset-0 after:z-50 after:[background:linear-gradient(to_top,hsl(var(--background))_30%,transparent)]"
+      >
+        <div
+          className={`rounded-xl border border-white/10 bg-white bg-opacity-[0.01] before:absolute before:bottom-1/2 before:left-0 before:top-0 before:h-full before:w-full before:opacity-0 before:[filter:blur(180px)] before:[background-image:linear-gradient(to_bottom,var(--color-one),var(--color-one),transparent_40%)] ${
+            inView ? "before:animate-image-glow" : ""
+          }`}
+        >
+          <BorderBeam
+            size={200}
+            duration={12}
+            delay={11}
+            colorFrom="var(--color-one)"
+            colorTo="var(--color-two)"
+          />
+
+          <img
+            src="/hero.png"
+            alt="Hero Image"
+            className="relative w-full h-full rounded-[inherit] object-contain"
+          />
+        </div>
+      </div>
+    </section>
+  );
+}
